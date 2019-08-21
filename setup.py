b76c4e1258751e9c5058097783f2296221c7e38c
John M. Cook <john-cook@uiowa.edu>
2019-08-20 20:13:09 -0500

    adding common utils

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("irlutils/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irlutils",
+    version="0.0.1",
+    author="The University of Iowa Internet Research Lab",
+    author_email="john-cook@uiowa.edu",
+    description="A set of useful utilities",
+    long_description_content_type="text/markdown",
+    url="https://github.com/uiowa-irl/uiowa-irl-utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
+)
