6db7ed2a57d26fb3ca89b9885a880987261b376c
John M. Cook <john-cook@uiowa.edu>
2019-09-26 20:27:40 -0500

    updated file utils

--- irlutils/file/file_utils.py
+++ irlutils/file/file_utils.py
@@ -6,8 +6,6 @@ import fnmatch
 import argparse
 import tempfile
 
-
-
 """
 References: 
 [1] Englehardt, Steven, and Arvind Narayanan. 
@@ -15,19 +13,38 @@ References:
     In Proceedings of the 2016 ACM SIGSAC Conference on Computer and 
     Communications Security, pp. 1388-1401. ACM, 2016.
 """
+__author__="senorchow"
+
+def gen_find_files(**kwargs):
+    """returns filenames that matches the given pattern under() a given dir
 
 
-def gen_find_files(filepat, top):
+    Kwargs:
+        file_pattern (str): a regex style string . 
+        root (str): top level folder to begin search from. 
+
+    Yields:
+        path: matching path str
+
+    Examples:
+        gen_find_files(file_pattern="*.sql", root="/mnt/data/).
+
+        >>> gen_find_files(file_pattern="*.sql", root="/mnt/data/)
+        /mnt/data/first_folder/last_folder/file.sqlite
+        
+    Reference: 
+        [1] http://www.dabeaz.com/generators/
     """
-    http://www.dabeaz.com/generators/
-    returns filenames that matches the given pattern under() a given dir
-    """
-    for path, _, filelist in os.walk(top):
-        for name in fnmatch.filter(filelist, filepat):
+
+    file_pattern = kwargs.get("file_pattern", "")
+    root = kwargs.get("root", "")
+    for path, _, filelist in os.walk(root):
+        for name in fnmatch.filter(filelist, file_pattern):
             yield os.path.join(path, name)
 
-def rmsubtree(location):
+def rmsubtree(**kwargs):
     """Clears all subfolders and files in location"""
+    location = kwargs.get("location", "")
     for root, dirs, files in os.walk(location):
         for f in files:
             os.unlink(os.path.join(root, f))
@@ -37,24 +54,42 @@ def rmsubtree(location):
 
 
 def tar_unpacker(**kwargs):
+    """ unpacks tar to a tmp directory. 
+
+
+    Kwargs:
+
+        tar_path (str): tar file path
+        versbose (bool): True enables verbose
+
+    Yields:
+
+        tmp_path: extracted contents path
+
+    Examples:
+
+        tar_unpacker(file_pattern="/mnt/data/tarfile.tar.gz").
+
+        >>> tar_unpacker(file_pattern="/mnt/data/tarfile.tar.gz").
+        /tmp/FZ4245_Zb/
+    """
     tar_path = kwargs.get('tar_path','')
-    num_files = len(os.listdir(tar_path))
+    verbose = kwargs.get("verbose", False)
     count=1
-    file_paths = {}
-    for file in os.listdir(tar_path):
-        tmp_path = tempfile.mkdtemp()
+    
+    tmp_path = tempfile.mkdtemp()
 
-        count+=1
-        path = os.path.join(tar_path, file)
-        file_paths[file] = path
-        tf = tarfile.open(path)
-        tf.extractall(path=tmp_path)
-    return file_paths
+    count+=1
+    command ="tar -xf {}".format(tar_path)
+
+    if verbose: 
+        command ="tar -xvf {}".format(tar_path)
+    os.system(command)
+    return tmp_path
 
 
-
-def json_flatten( **kwargs):
-    data = kwargs.get('data')
+def json_flatten(**kwargs):
+    data = kwargs.get('data',"")
     kv = kwargs.get('kv', {})
     try: 
         for key,value in data.items():
