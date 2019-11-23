6e7ca98a25abc1ceba18d73b81a40f013455b159
John M. Cook <john-cook@uiowa.edu>
2019-11-23 10:23:14 -0600

    irlutils release v0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="irlutils",
-    version="0.1.1",
+    version="0.1.3",
     author="The University of Iowa Internet Research Lab",
     author_email="john-cook@uiowa.edu",
     description="IRL Utilities",
